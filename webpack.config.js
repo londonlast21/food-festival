90b1c87dec5f9189f26e10c97bf535342e69606a
jack <biffisbuffm@gmail.com>
2020-10-01 15:02:20 -0500

    edits images

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,37 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const webpack = require("webpack");
 module.exports = {
-    entry: './assets/js/script.js',
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/event.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+  },
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'main.bundle.js'
+      filename: "[name].bundle.js",
+      path: __dirname + "/dist",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jpeg$/i,
+          use: [
+            {
+              loader: 'file-loader'
+            }
+          ]
+        }
+      ],
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static", // outputs HTML file to dist folder
+        })
     ],
     mode: 'development'
   };
